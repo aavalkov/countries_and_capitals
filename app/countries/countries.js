13b199260bee26fd06e175a78e3e2dca89e46f82
Anna Valkov <anna.a.valkov@gmail.com>
2015-12-15 20:00:43 -0800

    cache countries so the api call is only made once

--- app/countries/countries.js
+++ app/countries/countries.js
@@ -1,19 +1,28 @@
 angular.module('countriesApp')
-	.factory('getCountries', function($http){
-		return function(){
-			return $http.get('http://api.geonames.org/countryInfoJSON?username=aavalkov');
-		}
-	})
+	.factory('getCountries', ['$q', '$http', function($q, $http) {
+	  var c = {
+	    cache: null,
+	    get: function()  {
+	      if (!c.cache) {
+	        return $http.get('http://api.geonames.org/countryInfoJSON?username=aavalkov').then(function(response) {
+	          c.cache = response.data.geonames;
+	          return c.cache;
+	        });
+	      }
+	      var deferred = $q.defer();
+	      deferred.resolve(c.cache);
+	      return deferred.promise;
+	    }
+	  };
+	  return c;
+	}])
 	.controller('CountriesCtrl', ['$rootScope','$scope','getCountries','$location', function($rootScope, $scope, getCountries, $location){
-		getCountries().then(function(response) {
+		getCountries.get().then(function(response) {
 			$rootScope.isLoading = false;
-            $scope.countries = response.data.geonames;
+            $scope.countries = response;
         });
 
         $scope.showCountry = function(country){
         	$location.path('/countries/'+ country.countryCode + '/capital');
         }
 	}]);
-
-
-
